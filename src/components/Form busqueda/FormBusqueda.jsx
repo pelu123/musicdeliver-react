47b9003961abe6e5e3d7246049b0d69e289643d2
pelu123 <tomascooklastra@gmail.com>
2022-11-01 11:33:39 -0300

    "cambios"

--- src/components/Form busqueda/FormBusqueda.jsx
+++ src/components/Form busqueda/FormBusqueda.jsx
@@ -4,7 +4,7 @@ const Form = ({busqueda}) => {
     return (
         <form className="d-flex">
                         <input className="form-control me-2" type="search" placeholder={busqueda} aria-label="Search" />
-                        <button className="btn btn-outline-success" type="submit">Buscar productos</button>
+                        <button className="btn btn-outline-success" type="submit">Buscar producto</button>
                     </form>
     );
 }
